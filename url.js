2a2d88f825a82ba33d042763b653d0f8db339e00
105ecamia-coder <105ecamia@gmail.com>
2026-03-09 16:05:05 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://lin.ee/eCFWp17',
+	'https://lin.ee/UDUkdNp',
+	'https://lin.ee/r2GgHN4',
 ];
